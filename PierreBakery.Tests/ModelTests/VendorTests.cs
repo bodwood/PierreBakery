6936a6c59ce607649c1aeb3ff4a1e4d507d591df
bodwood <bodiewood@hotmail.com>
2022-12-16 13:05:35 -0800

    add test to VendorTests.cs to test the Vendor constructor type. Test successful.

--- PierreBakery.Tests/ModelTests/VendorTests.cs
+++ PierreBakery.Tests/ModelTests/VendorTests.cs
@@ -5,13 +5,20 @@ using System;
 
 namespace PierreBarkery.Tests
 {
-  // [TestClass]
-  // public class VendorTests : IDisposable
-  // {
+  [TestClass]
+   public class VendorTests //: IDisposable
+  {
 
-  //   // public void Dispose()
-  //   // {
-  //   //   Vendor.ClearAll();
-  //   // }
-  // }
+    // public void Dispose()
+    // {
+    //   Vendor.ClearAll();
+    // }
+
+    [TestMethod]
+    public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
+    {
+      Vendor newVendor = new Vendor("vendor test");
+      Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+  }
 }
--- PierreBakery/Models/Vendor.cs
+++ PierreBakery/Models/Vendor.cs
@@ -5,9 +5,21 @@ namespace PierreBarkery.Models
 {
   public class Vendor
   {
-    private static List<Vendor> _instances = new List<Vendor> {};
-    public string name {get; set;}  //name of vendor
-    public int Id {get;}  //unique id of vendor
-    public List<Order> Orders {get; set;} //list of orders (i.e. Matts BBQ -> Pulled Pork)
+    private static List<Vendor> _instances = new List<Vendor> { };
+    public string Name { get; set; }  //name of vendor
+    public int Id { get; }  //unique id of vendor
+    public List<Order> Orders { get; set; } //list of orders (i.e. Matts BBQ -> Pulled Pork)
+
+    public Vendor(string name)
+    {
+      Name = name;
+      _instances.Add(this);
+      Id = _instances.Count;
+    }
   }
+
+  // public static void ClearAll()
+  // {
+  //   _instances.Clear();
+  // }
 }
