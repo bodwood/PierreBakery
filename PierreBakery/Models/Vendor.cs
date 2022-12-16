6c1389139ff2e1d87525c2a08b4c573a4726fd36
bodwood <bodiewood@hotmail.com>
2022-12-16 12:57:50 -0800

    add Vendor class setup for getting and returning name, and a list of the orders

--- PierreBakery/Models/Vendor.cs
+++ PierreBakery/Models/Vendor.cs
@@ -5,6 +5,9 @@ namespace PierreBarkery.Models
 {
   public class Vendor
   {
-
+    private static List<Vendor> _instances = new List<Vendor> {};
+    public string name {get; set;}  //name of vendor
+    public int Id {get;}  //unique id of vendor
+    public List<Order> Orders {get; set;} //list of orders (i.e. Matts BBQ -> Pulled Pork)
   }
 }
