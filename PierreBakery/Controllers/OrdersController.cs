986d27fa6e8292a65b174ab26d763a2e60cebff3
bodwood <bodiewood@hotmail.com>
2022-12-16 12:19:00 -0800

    add content to cs.html files in order to pages to open. Routes were created for each cs.html file.

--- PierreBakery/Controllers/OrdersController.cs
+++ PierreBakery/Controllers/OrdersController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using PierreBarkery.Models;
+using System.Collections.Generic;
 
 namespace PierreBarkery.Controllers
 {
@@ -8,6 +10,27 @@ namespace PierreBarkery.Controllers
     [HttpGet("/orders")]
     public ActionResult Index()
     {
+      List<Order> starterOrder = Order.GetAll();
+      return View(starterOrder);
+    }
+
+    [HttpGet("/orders/new")]
+    public ActionResult CreateForm()
+    {
+      return View();
+    }
+
+    [HttpPost("/orders")]
+    public ActionResult Create(string description)
+    {
+      Order myOrder = new Order(description);
+      return RedirectToAction("Index");
+    }
+
+    [HttpPost("/orders/delete")]
+    public ActionResult DeleteAll()
+    {
+      Order.ClearAll();
       return View();
     }
   }
